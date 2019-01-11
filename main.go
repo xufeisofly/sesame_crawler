c23720b1ffc21aac631e7159bad657d3f6606b9b
Norris <feixu@tju.edu.cn>
2019-01-11 13:31:47 +0800

    db config

--- main.go
+++ main.go
@@ -12,31 +12,9 @@ import (
 
 const (
 	user    = "norris"
-	dbname  = "sesame_development"
+	dbname  = "sesame_production"
 	sslmode = "disable"
 )
-
-var destinations = []interface{}{
-	"石家庄",
-	"天津",
-	"南京",
-	"广州",
-	"哈尔滨",
-	"沈阳",
-	"长春",
-	"呼和浩特",
-	"郑州",
-	"济南",
-	"杭州",
-	"上海",
-	"厦门",
-	"成都",
-	"重庆",
-	"拉萨",
-	"乌鲁木齐",
-	"西宁",
-	"昆明",
-}
 
 func main() {
 	// Init DB
