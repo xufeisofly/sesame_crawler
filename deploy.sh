ad90ecd5029450b6331c48015986406400bf53fc
Norris <feixu@tju.edu.cn>
2019-01-11 13:33:33 +0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Deploying..."
-rsync -a --exclude='/.git' ../sesame ubuntu@118.89.236.14:~/go/src
+rsync -a --exclude='/.git' ../sesame ubuntu@niltouch.cn:~/go/src
 echo "Done!"
