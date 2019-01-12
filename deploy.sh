90b08930ddd3c7c0e60b3080bdc84d2b5df89681
Norris <feixu@tju.edu.cn>
2019-01-12 22:43:09 +0800

    gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Deploying..."
-rsync -a --exclude='/.git' --exclude='.env' ../sesame ubuntu@niltouch.cn:~/go/src
+echo "Deploying start"
+echo "Rsync files..."
+rsync -a --exclude='/.git' --exclude='.env' --exclude='/vendor' ../sesame ubuntu@niltouch.cn:~/go/src
 echo "Done!"
+echo "Running Local Script..."
+ssh -t ubuntu@niltouch.cn 'cd go/src; bash -s' < local.sh
+echo "Done!"
+echo "Deploy Success!"
